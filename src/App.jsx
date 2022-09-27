354bd3ca32bb7eb4d9ce51aab478edf3bea6324c
Thompson Plyler <nealok.g@gmail.com>
2022-09-27 09:58:09 -0400

    Implemented changes with createBrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { UserProvider } from './context/userDetails'
-import SetUserDetails from './routes/SetUserDetails'
-import FetchUserDetails from './routes/FetchUserDetails'
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import { UserProvider } from "./context/userDetails";
+import SetUserDetails from "./routes/SetUserDetails";
+import FetchUserDetails from "./routes/FetchUserDetails";
 
-// Route that goes to let us assign a user. 
+// Route that goes to let us assign a user.
 // When we click the button that assigns the user
-// Go to user's start page. 
+// Go to user's start page.
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <SetUserDetails />,
+  },
+  { path: "/user", element: <FetchUserDetails /> },
+]);
 
 function App() {
-
   return (
     <UserProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<SetUserDetails />} />
-          <Route path="/user" element={<FetchUserDetails />} />
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </UserProvider>
-  )
+  );
 }
 
-export default App
+export default App;
